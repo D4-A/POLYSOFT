ac97d44b729f3884169518344f8c9b2d7facfd31
guy fleury iteriteka <gfleury@disroot.org>
2020-07-28 19:32:22 +0200

    Add seed user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('isAdmin', function($user) {
+            return $user->role == 'admin';
+         });
+        
+         //define a author user role 
+         Gate::define('isSimpleuser', function($user) {
+             return $user->role == 'simpleuser';
+         });
     }
 }
