9de61111d90a1acbb1870d377a8b0cdf5614084d
guy fleury iteriteka <gfleury@disroot.org>
2020-08-01 12:17:07 +0200

    dynamicaly choose search key

--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,16 @@
 function incrementalsearch(inputname,tablename) {
     
     let input, filter, table, tr, td, i, txtValue;
+
+    let index = document.getElementById('search').value;
+    
     input = document.getElementById(inputname,tablename);
     filter = input.value.toUpperCase();
     table = document.getElementById(tablename);
     tr = table.getElementsByTagName("tr");
     
     for (i = 0; i < tr.length; i++) {
-	td = tr[i].getElementsByTagName("td")[1];
+	td = tr[i].getElementsByTagName("td")[index];
 	if (td) {
 	    txtValue = td.textContent || td.innerText;
 	    if (txtValue.toUpperCase().indexOf(filter) > -1) {
--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -6,7 +6,6 @@
 	<head>
 	    <meta charset="UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <title>Polyclinique du Nord</title>
 	</head>
 	<body>
 	    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
@@ -30,7 +29,11 @@
 			Nouveau service
 		    </button>
 		</a>
-		<input type="text" id="input" onkeyup="incrementalsearch('input','table')" placeholder="Search for names..">
+		<select class="selectpicker" id="search">
+		    <option value="0"> ID</option>
+		    <option value="1"> Nom</option>
+		</select>
+		<input class="input" type="text" id="input" onkeyup="incrementalsearch('input','table')" placeholder="Search for names..">
 		<table class="table" id="table">
 		    <thead>
 			<tr>
