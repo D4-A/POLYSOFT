e24a06a0e3133eeae00d53c1325546550947a5b8
guy fleury iteriteka <gfleury@disroot.org>
2020-08-02 15:59:31 +0200

    Add fillfiles javascript file

---
+++ public/js/fillfiles.js
@@ -0,0 +1,46 @@
+$.ajaxSetup({
+    
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')	
+    } 
+});
+
+var a = document.getElementById('files');
+
+function basename(path) {
+   return path.split('/').reverse()[0];
+}
+
+function create_checkbox(balise,file){
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "filepath2[]";
+    checkbox.value = file;
+    checkbox.id = "id";
+
+    var label = document.createElement('label')
+    label.htmlFor = "id";
+    label.appendChild(document.createTextNode(basename(file)));
+    
+    balise.appendChild(checkbox);
+    balise.appendChild(label);
+
+}
+$("#cons_id").on('change', function(e){
+
+    $('form').attr('action', '/sendp');
+    
+    $.ajax({
+	data: {cons_id: 1},
+	dataType: 'json',
+        type:'GET',
+        url:"/ajaxfiles",
+	success : function(data, statut){
+            data.forEach(item => create_checkbox(a,item));
+       },
+
+       error : function(resultat, statut, erreur){
+	   console.log(erreur);
+       }
+    });
+});
