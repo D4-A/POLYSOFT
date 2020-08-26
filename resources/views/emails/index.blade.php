3a241ed3876b2885b653be6b4c8490c59ac9bb8d
Brice NGABIRANO <ngablanco234@gmail.com>
2020-08-26 17:26:04 +0200

    modification title

--- resources/views/emails/index.blade.php
+++ resources/views/emails/index.blade.php
@@ -1,5 +1,5 @@
 @extends('templates.default_layout')
-@section('title', 'listes des emails')
+@section('title', 'LISTE DES E-MAILS')
 @section('content')
     <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 	<div class="row">
