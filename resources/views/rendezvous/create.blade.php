5bfd3c93a7b92b8c8668dbceb5b3dd6b6c694a8a
Brice NGABIRANO <ngablanco234@gmail.com>
2020-08-26 17:24:35 +0200

    modification title

--- resources/views/rendezvous/create.blade.php
+++ resources/views/rendezvous/create.blade.php
@@ -1,5 +1,5 @@
 @extends('templates.default_layout')
-@section('title', 'Ajout d\'un rendez-vous')
+@section('title', 'AJOUTER RE-VOUS')
 @section('content')
     <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 	<div class="row">
