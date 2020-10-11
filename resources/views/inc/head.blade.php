f2a174162d7446738c733b1658edfd1d3c225fec
Brice NGABIRANO <ngablanco234@gmail.com>
2020-10-11 20:40:32 +0200

    add icon title,logo and designing other things

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
+	<link rel="icon" type="image/png" href="{{asset('plogin/images/icons/title.ico')}}"/>
 	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 	<link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
 	<link href="{{asset('css/datepicker3.css')}}" rel="stylesheet">
--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -1,14 +1,17 @@
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
-					<span class="icon-bar"></span></button>
-				<a class="navbar-brand" href="#"><span>Polyclinique du Nord</span> Software</a>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#" style="padding:0px;">
+					<span><img src="{{asset('plogin/images/log2.png')}}" style="width:225px;height:155px;"/><span>
+				</a>
 				<ul class="nav navbar-top-links navbar-right">
 				    <li class="dropdown"><a href="{{url('/rendezVous')}}">
-					<em class="fa fa-calendar"></em>
+					<em class="fa fa-calendar"></em>Agenda
 				    </a>
 				    
 				    </li>
