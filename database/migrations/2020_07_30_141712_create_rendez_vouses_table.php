ea96716c136a40c80bb37d7bf620c15a96a5c89c
gfleury <gfleury@disroot.org>
2020-10-31 17:14:28 +0200

    rendez-vous: use a custom `id`

--- database/migrations/2020_07_30_141712_create_rendez_vouses_table.php
+++ database/migrations/2020_07_30_141712_create_rendez_vouses_table.php
@@ -14,20 +14,20 @@ class CreateRendezVousesTable extends Migration
     public function up()
     {
         Schema::create('rendez_vouses', function (Blueprint $table) {
-            $table->id();
-            $table->string('patient_id');
-            $table->string('payement_id')->unique();
-            $table->string('creneau_id')->unique();
-            $table->string('description')->nullable();
-            $table->string('etat');
-            $table->timestamps();
-            
-            $table->foreign('patient_id',45)
-                  ->references('id')->on('patients')->onDelete('cascade');
-            $table->foreign('payement_id',42)
-                  ->references('id')->on('payements')->onDelete('cascade');
-            $table->foreign('creneau_id',48)
-                  ->references('id')->on('creneaus')->onDelete('cascade');
+                $table->string('id',32)->index();
+                $table->string('patient_id');
+                $table->string('payement_id')->unique();
+                $table->string('creneau_id')->unique();
+                $table->string('description')->nullable();
+                $table->string('etat');
+                $table->timestamps();
+                
+                $table->foreign('patient_id',45)
+                      ->references('id')->on('patients')->onDelete('cascade');
+                $table->foreign('payement_id',42)
+                      ->references('id')->on('payements')->onDelete('cascade');
+                $table->foreign('creneau_id',48)
+                      ->references('id')->on('creneaus')->onDelete('cascade');
             });
     }
 
