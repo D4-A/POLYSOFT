cbc8df28c323894626c4d103e89cc60255fd261f
gfleury <gfleury@disroot.org>
2020-10-31 16:47:24 +0200

    examens: use custom `id`

--- app/Examen.php
+++ app/Examen.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 
 class Examen extends Model
 {
-    //
+    protected $keyType = 'string';
+    public $incrementing = false;
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+                $model->id = IdGenerator::generate
+                           (['table' => 'examens',
+                             'length' => 10, 'prefix' => 'EX']);
+            });
+    }
 }
