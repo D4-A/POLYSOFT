b8562da8797af99749750b9d581dab62d7e258ae
gfleury <gfleury@disroot.org>
2020-11-02 08:41:43 +0200

    fix typo

--- resources/views/examens/index.blade.php
+++ resources/views/examens/index.blade.php
@@ -15,7 +15,7 @@
 
 	<div class="row">
 	    <div class="col-lg-12">
-		<h1 class="page-header">Ajouter Examen</h1>
+		<h1 class="page-header">Examens Effectues</h1>
 	    </div>
 	</div><!--/.row--><br>
 
